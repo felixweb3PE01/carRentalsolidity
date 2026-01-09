112896112c8ef5df8a2d2abc0c59931fa12afa2b
Felix <felixweb3PE@gmail.com>
2026-01-09 11:19:07 +0100

    Add files via upload

---
+++ carRental.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+
+
+error Insufficientfund();
+error Rented();
+error UnauthorizedAccess();
+error PriceNotAdded();
+
+contract carRental {
+
+    uint256 public balance ;
+    address owner;
+    bool public  isrented ;
+    uint256 public immutable  rentalprice;
+    address [] renters;
+    address currentRenter;
+
+    constructor(uint256 _rentalprice)payable{
+        owner = msg.sender;
+        rentalprice= _rentalprice;
+        if(_rentalprice == 0){
+            revert PriceNotAdded();
+        }
+        balance = msg.value;
+    }
+
+    function rentalAddressbook() public view returns (address[] memory) {
+        return renters;
+
+    }
+
+    
+
+    function rentCar() public payable  returns(string memory){
+        if(msg.value < rentalprice){
+            revert Insufficientfund();
+        }
+        if(isrented==true){
+            revert Rented();
+        }
+        isrented = true;
+        renters.push(msg.sender);
+        currentRenter = msg.sender;
+
+        return string( "Car Rental Succesful");
+
+    }
+    function returnCar()public {
+        if (currentRenter != msg.sender){
+            revert UnauthorizedAccess();
+        }
+        isrented = false;
+    }
+
+    function withdraw()public restrict{
+       
+    }
+
+    modifier restrict(){
+         if(msg.sender != owner){
+            revert UnauthorizedAccess();
+        }
+        _;
+    }
+    }
